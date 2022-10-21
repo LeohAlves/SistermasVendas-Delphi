183ec1175b7e28500f281b9b148f8e34dae41f52
Leonardo Alves <leonardoalves72@hotmail.com>
2022-10-20 22:18:34 -0300

    database conn begin
    comecando o conexao com o banco e adiçao da dll no projeto

--- prjVenda.dpr
+++ prjVenda.dpr
@@ -3,7 +3,8 @@ program prjVenda;
 uses
   Vcl.Forms,
   untPrincipal in 'untPrincipal.pas' {frmPrincipal},
-  untPadrao in 'untPadrao.pas' {frmPadrao};
+  untPadrao in 'untPadrao.pas' {frmPadrao},
+  untDtModel in 'untDtModel.pas' {Modulo: TDataModule};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmPrincipal, frmPrincipal);
   Application.CreateForm(TfrmPadrao, frmPadrao);
+  Application.CreateForm(TModulo, Modulo);
   Application.Run;
 end.
